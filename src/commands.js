89e6671a9a7687e3e00180ca2a7d1a3d8137b54a
Christopher Medlin <cjmedlin99@yahoo.com>
2018-04-13 11:48:22 -0700

    Create CommandRunner superclass for FakeTerminal and CanvasCommandInput

--- src/commands.js
+++ src/commands.js
@@ -2,6 +2,20 @@ import FakeTerminal from './terminal.js';
 import VimCanvas from './canvas.js';
 import $ from 'jquery';
 
+export class CommandRunner {
+    constructor(commands={}) {
+        this.commands = commands;
+    }
+
+    runCommand(input) {
+        input = input.split(" ");
+        if (input[0] in this.commands)
+            this.commands[input[0]](this, input.slice(1));
+        else
+            return "'" + input[0] + "' is not a command. Type 'help' for more information.";
+    }
+}
+
 function getCanvases_(success) {
     $.ajax({
         url: "https://api.vimcanvas.christophermedlin.me/v1/canvases",
