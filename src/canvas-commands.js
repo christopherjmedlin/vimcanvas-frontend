051a644ca6a5e992754dfade2432daf03c8ca321
Christopher Medlin <cjmedlin99@yahoo.com>
2018-04-21 16:55:04 -0700

    Add move command

--- src/canvas-commands.js
+++ src/canvas-commands.js
@@ -4,10 +4,16 @@ function quit(runner, args) {
     runner.terminal.focus();
 }
 
+function move(runner, args) {
+    runner.canvas.playerPos[0] = Number(args[0])
+    runner.canvas.playerPos[1] = Number(args[1])
+}
+
 let canvasCommands = {
     "q": quit,
     "wq": quit,
-    "x": quit
+    "x": quit,
+    "move": move
 };
 
 export default canvasCommands;
