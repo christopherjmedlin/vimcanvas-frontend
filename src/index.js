40fe0077e15778ba0e59c12cad3f3523abb564e0
Christopher Medlin <cjmedlin99@yahoo.com>
2018-05-14 19:09:08 -0700

    Change pathname check to include /index.html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function main() {
     let mainDiv = document.getElementById("mainDiv");
     mainDiv.style.height = window.innerHeight - 210 + "px";
     
-    if (location.pathname == '/') {
+    if (location.pathname == '/' || location.pathname == '/index.html') {
         let term = new FakeTerminal("mainDiv", '$>', commands);
         term.init();
 
